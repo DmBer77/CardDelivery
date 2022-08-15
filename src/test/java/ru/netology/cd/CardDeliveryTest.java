5d7e24a563c709b9fd0c983ab1aac3c8a1e24f5d
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-08-15 16:18:49 +0300

    add some change

--- src/test/java/ru/netology/cd/CardDeliveryTest.java
+++ src/test/java/ru/netology/cd/CardDeliveryTest.java
@@ -1,3 +1,26 @@
 package ru.netology.cd;
-public class CardDeliveryTest {
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+class CardDeliveryTest {
+
+    @Test
+    void shouldRegisterByAccountNumberDOMModification() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $x("//*[contains(text(),'номеру счёта')]").click();
+        $$("[type='text']").filter(visible).first().setValue("4055 0100 0123 4613 8564");
+        $$("[name='phone']").exclude(hidden).get(0).setValue("+7 999 999 99 99");
+        $$(By.className("button__content")).last().click();
+        $x("//*[contains(text(),'Успешная авторизация')]").shouldBe(visible, Duration.ofSeconds(10));
+        $(By.tagName("h2")).shouldHave(exactText("Личный кабинет"), Duration.ofMillis(8000));
+    }
+
 }
